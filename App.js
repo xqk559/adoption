a5ff63747094a589e8e54d3f1d8b51d154992784
xqk559 <44305153+xqk559@users.noreply.github.com>
2018-11-27 10:23:55 -0500

    Webscraper works

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import jsonData from './output.json';
 const zips = require('zips');
 
 class App extends Component {
   state = {
+    json: jsonData,
     zip: 21822,
     x: 21822
   }
@@ -23,6 +25,7 @@ class App extends Component {
     }
     let z = zips.getByZipCode(this.state.x);
     console.log(z);
+    let json = this.state.json.title;
     return (
       <div className="App">
       <div>
@@ -35,6 +38,7 @@ class App extends Component {
         <button onClick={buttonHandler}>Search Puppies!</button>
         <button onClick={buttonHandler}>Search Kitties!</button>
         <p>{z.city}</p>
+        <p>{json}</p>
       </div>
       </div>
     );
